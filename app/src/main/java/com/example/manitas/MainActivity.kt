3b74d073d486e3be7b1cc3a90fa157fed42b7b6c
Meminbb <kevinbolanos22@gmail.com>
2025-11-09 19:41:39 -0600

    Commit 1 - Start 0

--- app/src/main/java/com/example/manitas/MainActivity.kt
+++ app/src/main/java/com/example/manitas/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.*
+import androidx.navigation.compose.rememberNavController
 import com.example.manitas.ui.theme.ManitasTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ManitasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ManitasTheme {
-        Greeting("Android")
     }
 }
