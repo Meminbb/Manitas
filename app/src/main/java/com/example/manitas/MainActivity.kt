2ce4125d4fd8b30723744dd90d5c85a8998844ba
Karla Rosas <a00839680@tec.mx>
2025-11-11 13:48:55 -0600

    Prueba

--- app/src/main/java/com/example/manitas/MainActivity.kt
+++ app/src/main/java/com/example/manitas/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name! FUNCIONA?",
+        text = "Hello $name! FUNCIONA????",
         modifier = modifier
     )
 }
